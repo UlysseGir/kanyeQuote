2edecf12a4f252d0781d7b1661f7a5ba6beba123
lecanardjaune <ugiraud@edenschool.fr>
2022-04-20 11:29:50 +0200

    btn add random quotes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-fetch("https://api.kanye.rest/")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.quote)
-    
-  })
-  .catch((error) => console.log(error));
+const btn = document.getElementById("btn");
+const quotes = document.getElementById("quotes")
+
+function newQuote(){
+    fetch("https://api.kanye.rest/")
+        .then((response) => response.json())
+        .then((data) => {
+            const quote = document.createElement("div")
+            quotes.appendChild(quote);
+            quote.textContent = data.quote;
+    }).catch((error) => console.log(error));  
+}
+
+btn.addEventListener("click", function(){
+    newQuote();
+})
