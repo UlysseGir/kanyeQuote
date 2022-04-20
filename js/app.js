83d83b8ec941a82a3a2dd1577485b238341db385
lecanardjaune <ugiraud@edenschool.fr>
2022-04-20 11:50:48 +0200

    no double quotes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 const btn = document.getElementById("btn");
 const quotes = document.getElementById("quotes")
+let double = [];
 
 function newQuote(){
     fetch("https://api.kanye.rest/")
         .then((response) => response.json())
         .then((data) => {
-            const quote = document.createElement("div")
-            quotes.appendChild(quote);
-            quote.textContent = data.quote;
+            if(double.includes(data.quote)){
+                newQuote();
+            } else {
+                const quote = document.createElement("div")
+                quotes.appendChild(quote);
+                quote.textContent = data.quote;
+            }
+            double.push(data.quote);
     }).catch((error) => console.log(error));  
 }
 
