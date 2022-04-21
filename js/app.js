ca6da99481a269088896bf6399239cf03747e828
lecanardjaune <ugiraud@edenschool.fr>
2022-04-21 17:36:36 +0200

    btn up and down finish

--- js/app.js
+++ js/app.js
@@ -28,20 +28,21 @@ function newQuote(){
                 down.className = "down";
                 down.textContent = "down";
 
-                const btnUps = document.getElementsByClassName("up")
-                const btnDowns = document.getElementsByClassName("down")
-                for(let btnUp of btnUps){
-                    btnUp.addEventListener("click", function(){
-                        quote.insertAdjacentElement("beforebegin", (btnUp.parentElement).previousElementSibling)
+                const btnUps = document.getElementsByClassName("up");
+                const btnDowns = document.getElementsByClassName("down");
+                const btnUp = btnUps[btnUps.length-1];
+                const btnDown = btnDowns[btnDowns.length-1];
 
-                    })
-                }
-                for(let btnDown of btnDowns){
-                    btnDown.addEventListener("click", function(){
-                        quote.insertAdjacentElement("afterbegin", (btnDown.parentElement).nextElementSibling)
+                btnUp.addEventListener("click", function(){
+                    (btnUp.parentElement).previousElementSibling.insertAdjacentElement("beforebegin", btnUp.parentElement);
+                    console.log(btnUp.parentElement)
+                })
+                
+                btnDown.addEventListener("click", function(){
+                    (btnDown.parentElement).nextElementSibling.insertAdjacentElement("afterend", btnDown.parentElement);
+                    console.log(quote)
+                })
 
-                    })
-                }
             }
             
     }).catch((error) => console.log(error));  
